1a9d8387ddc8dcbd73983a53fd50b8e5171709b5
funzter <funzter@mail.timesnake.de>
2022-12-26 18:11:39 +0100

    Updated copyright

--- src/main/java/de/timesnake/extension/proxy/cmd/mail/MailHandler.java
+++ src/main/java/de/timesnake/extension/proxy/cmd/mail/MailHandler.java
@@ -1,19 +1,5 @@
 /*
- * extension-proxy.main
  * Copyright (C) 2022 timesnake
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; If not, see <http://www.gnu.org/licenses/>.
  */
 
 package de.timesnake.extension.proxy.cmd.mail;
@@ -32,11 +18,10 @@ import de.timesnake.database.util.user.DbUserMail;
 import de.timesnake.library.basic.util.chat.ExTextColor;
 import de.timesnake.library.extension.util.chat.Code;
 import de.timesnake.library.extension.util.cmd.ChatDivider;
-import net.kyori.adventure.text.Component;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.UUID;
+import net.kyori.adventure.text.Component;
 
 public class MailHandler {
 
